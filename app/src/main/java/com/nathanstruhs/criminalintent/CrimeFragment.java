f617250c0a8ca401d92798a4486e7884b53e6820
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-06 13:32:07 -0700

    Ch7 pg144: create CrimeFragment class
    Override onCreate and onCreateView methods

---
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeFragment.java
@@ -0,0 +1,23 @@
+package com.nathanstruhs.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
