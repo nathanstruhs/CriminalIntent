eaed0a03efb6860346f6d732d2ea6d4366852ff0
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-08 13:18:08 -0700

    Ch8 pg161: add 100 fake crimes to mCrimes

--- app/src/main/java/com/nathanstruhs/criminalintent/CrimeLab.java
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeLab.java
@@ -2,8 +2,14 @@ package com.nathanstruhs.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -13,6 +19,21 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
     }
 }
