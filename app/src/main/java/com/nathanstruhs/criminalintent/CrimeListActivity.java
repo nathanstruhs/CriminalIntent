fffa1c7dcffd12f354b8f39cec0995a8fdd9629f
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-08 13:34:46 -0700

    Ch8 pg 166:add new controllers & CrimeListActivity

---
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.nathanstruhs.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.nathanstruhs.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListFragment extends Fragment {
+
+    // nothing yet
+
+}
