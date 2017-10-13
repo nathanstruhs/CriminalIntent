5870e7c9eee529c9c482216541669de1c902edaf
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-13 09:05:11 -0700

    Ch8 pg175: Implement ViewHolder and Adapter
    For the CrimeListFragment.

--- app/src/main/java/com/nathanstruhs/criminalintent/CrimeLab.java
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeLab.java
@@ -28,6 +28,10 @@ public class CrimeLab {
         }
     }
 
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
     public Crime getCrime(UUID id) {
         for (Crime crime : mCrimes) {
             if (crime.getId().equals(id)) {
