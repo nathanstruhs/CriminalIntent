4e1764a2818354180ddc0963a852d8bb16b80076
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-17 19:45:39 -0700

    CHALLENGE: add first and last buttons
    Configure buttons in activity_crime_pager.xml, set onclick listeners to
    set the current item of the ViewPager to the respective end of the list.
    Create an OnPageChangeListner to check if the current position is the
    first or last and then disable the appropriate button.

--- app/src/main/java/com/nathanstruhs/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimePagerActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,6 +19,8 @@ public class CrimePagerActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+    private Button mFirstButton;
+    private Button mLastButton;
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimePagerActivity.class);
@@ -32,6 +36,20 @@ public class CrimePagerActivity extends AppCompatActivity {
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mFirstButton = (Button) findViewById(R.id.first_button);
+        mFirstButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        mLastButton = (Button) findViewById(R.id.last_button);
+        mLastButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+            }
+        });
 
         mCrimes = CrimeLab.get(this).getCrimes();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -54,5 +72,27 @@ public class CrimePagerActivity extends AppCompatActivity {
                 break;
             }
         }
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position == 0) {
+                    mFirstButton.setEnabled(false);
+                } else {
+                    mFirstButton.setEnabled(true);
+                }
+                if (position == mCrimes.size() - 1) {
+                    mLastButton.setEnabled(false);
+                } else {
+                    mLastButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
     }
 }
