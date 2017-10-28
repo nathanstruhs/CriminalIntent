75aab637960e210f2d377b8530f128306bfd0d95
Nathan Struhs <nathanstruhs@gmail.com>
2017-10-28 11:05:50 -0700

    Ch14 p286: ensure data persistence

--- app/src/main/java/com/nathanstruhs/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/nathanstruhs/criminalintent/CrimeListFragment.java
@@ -104,6 +104,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyDataSetChanged();
         }
     }
@@ -163,6 +164,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
     }
 }
